86dd5b0ffd634271b53997483b9c3c569ddc3bed
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-20 13:16:30 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(driverarr, name ){
+    return driverarr.filter(drivers => drivers.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(driversarr, name){
+    return driversarr.filter(drivers => drivers.toLowerCase().startsWith(name.toLowerCase()))
+}
+
+function matchName(arr, name){
+    return arr.filter(drivers => drivers.name.toLowerCase() === name.toLowerCase())
+}
